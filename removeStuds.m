d30305369b0fb9343adee857b4500892a7ba5e56
Michael Krumin <mkrumin@gmail.com>
2022-07-06 15:42:02 +0100

    Added removeStuds.m function

---
+++ removeStuds.m
@@ -0,0 +1,35 @@
+function removeStuds(path)
+
+% will recursively remove all the empty subfolders of path 
+if nargin < 1
+    path = 'D:\ProcessingTmp';
+end
+list = dir(path);
+for iList = 1:numel(list)
+    if list(iList).isdir 
+        if isequal(list(iList).name, '.') || isequal(list(iList).name, '..')
+            continue;
+        end
+        rmdirRec(fullfile(list(iList).folder, list(iList).name)); 
+        success = rmdir(fullfile(list(iList).folder, list(iList).name));
+    end
+end
+
+end
+
+
+function rmdirRec(dirPath)
+% will recursively remove all the empty subfolders of dirPath 
+% and then the dirPath itself as well 
+list = dir(dirPath);
+for iList = 1:numel(list)
+    if list(iList).isdir 
+        if isequal(list(iList).name, '.') || isequal(list(iList).name, '..')
+            continue;
+        end
+        rmdirRec(fullfile(list(iList).folder, list(iList).name));
+        success = rmdir(fullfile(list(iList).folder, list(iList).name));
+    end
+end
+
+end
